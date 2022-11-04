f39e05bf1dcb34f212797c7ab9be575ee5dc44e0
Taner Hacıoğlu <tanerfb1999@gmail.com>
2022-11-04 18:05:17 +0300

    added invite, request and member services

--- routes/project.js
+++ routes/project.js
@@ -65,4 +65,113 @@ router.post("/", async (req, res) => {
   }
 });
 
+// send request to project
+router.post("/request", async (req, res) => {
+  try {
+    const { userId, projectId } = req.body;
+
+    const isMember = await pool.query(
+      "SELECT * FROM member WHERE user_id = $1 AND project_id = $2",
+      [userId, projectId]
+    );
+
+    if (isMember.rows.length !== 0) {
+      return res
+        .status(401)
+        .json("Can't send a request. User is already a member.");
+    }
+
+    const newRequest = await pool.query(
+      "INSERT INTO request (user_id, project_id) VALUES ($1, $2) RETURNING *",
+      [userId, projectId]
+    );
+
+    res.json(newRequest.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// send invite to user
+router.post("/invite", async (req, res) => {
+  try {
+    const { userId, projectId } = req.body;
+
+    const isMember = await pool.query(
+      "SELECT * FROM member WHERE user_id = $1 AND project_id = $2",
+      [userId, projectId]
+    );
+
+    if (isMember.rows.length !== 0) {
+      return res
+        .status(401)
+        .json("Can't send an invite. User is already a member.");
+    }
+
+    const invite = await pool.query(
+      "INSERT INTO invite (user_id, project_id) VALUES ($1, $2) RETURNING *",
+      [userId, projectId]
+    );
+
+    res.json(invite.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// add user to project
+router.post("/member", async (req, res) => {
+  try {
+    const { userId, projectId } = req.body;
+
+    // check if user is already member
+    const isMember = await pool.query(
+      "SELECT * FROM member WHERE user_id = $1 AND project_id = $2",
+      [userId, projectId]
+    );
+
+    if (isMember.rows.length !== 0) {
+      return res
+        .status(401)
+        .json("Can't send an invite. User is already a member.");
+    }
+
+    // add user to project
+    const member = await pool.query(
+      "INSERT INTO member (user_id, project_id) VALUES ($1, $2) RETURNING *",
+      [userId, projectId]
+    );
+
+    // if user has invite delete invite
+    const hasInvite = await pool.query(
+      "SELECT * FROM invite WHERE user_id = $1 AND project_id = $2",
+      [userId, projectId]
+    );
+
+    if (hasInvite.rows.length !== 0) {
+      await pool.query(
+        "DELETE FROM invite WHERE user_id = $1 AND project_id = $2",
+        [userId, projectId]
+      );
+    }
+
+    // if user has request delete request
+    const hasRequest = await pool.query(
+      "SELECT * FROM request WHERE user_id = $1 AND project_id = $2",
+      [userId, projectId]
+    );
+
+    if (hasRequest.rows.length !== 0) {
+      await pool.query(
+        "DELETE FROM request WHERE user_id = $1 AND project_id = $2",
+        [userId, projectId]
+      );
+    }
+
+    res.json(member.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 module.exports = router;
